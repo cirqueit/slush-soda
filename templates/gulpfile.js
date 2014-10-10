a1f750f39a74f2963d888504c25ed9e6c0998782
Joe Edwards <handbalancing@gmail.com>
2014-10-10 03:00:24 -0700

    updated flatui, as well as removed tiny-lr, and added fui-angular

--- templates/gulpfile.js
+++ templates/gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     http = require('http'),
-    clean = require('gulp-rimraf'),
+    del = require('del'),
     plumber = require('gulp-plumber'),
     jade = require('gulp-jade'),
     stylus = require('gulp-stylus'),
@@ -9,43 +9,42 @@ var gulp = require('gulp'),
     coffee = require('gulp-coffee'),
     livereload = require('gulp-livereload'),
     embedlr = require('gulp-embedlr'),
-    lr = require('tiny-lr'),
     express = require('express'),
-    reloadServer = lr(),
     ip = require('ip');
 
 var appPort = 9000,
     lrPort = 35728,
+    lrOpt = {port: lrPort},
     location = { hostname: ip.address() },
     inlineOpt = { minify: false },
     plumberOpt = { errorHandler: function (err) { gutil.beep(); gutil.log(err);}};
 
 gulp.task('clean', function() {
-    return gulp.src('public/*')
-           .pipe(clean());
+    return del.sync(['public/*']);
+         
 });
 
 gulp.task('extern', function() {
     return gulp.src(['src/assets/**/*', 'src/vendor/**/*'])
            .pipe(gulp.dest('public/assets/'))
-           .pipe(livereload(reloadServer));
+           .pipe(livereload(lrPort));
 });
 
 gulp.task('jade',  function() {
     return gulp.src('src/**/*.jade')
            .pipe(plumber(plumberOpt))
            .pipe(jade({pretty: true}))
-           .pipe(embedlr({port: lrPort}))
+           .pipe(embedlr(lrOpt))
            .pipe(gulp.dest('public/'))
-           .pipe(livereload(reloadServer));
+           .pipe(livereload(lrPort));
 });
 
 gulp.task('html', function() {
     return gulp.src('src/**/*.html')
            .pipe(plumber(plumberOpt))
-           .pipe(embedlr({port: lrPort}))
+           .pipe(embedlr(lrOpt))
            .pipe(gulp.dest('public/'))
-           .pipe(livereload(reloadServer));
+           .pipe(livereload(lrPort));
 });
 
 gulp.task('coffee', function() {
@@ -55,14 +54,14 @@ gulp.task('coffee', function() {
            .pipe(coffee({bare: true}))
            .pipe(sourcemaps.write('../maps'))
            .pipe(gulp.dest('public/scripts/'))
-           .pipe(livereload(reloadServer));
+           .pipe(livereload(lrPort));
 });
 
 gulp.task('scripts', function() {
     return gulp.src('src/scripts/**/*.js')
            .pipe(plumber(plumberOpt))
            .pipe(gulp.dest('public/scripts/'))
-           .pipe(livereload(reloadServer));
+           .pipe(livereload(lrPort));
 });
 
 gulp.task('stylus', function() {
@@ -70,14 +69,14 @@ gulp.task('stylus', function() {
            .pipe(plumber(plumberOpt))
            .pipe(stylus({error: true}))
            .pipe(gulp.dest('public/styles/'))
-           .pipe(livereload(reloadServer));
+           .pipe(livereload(lrPort));
 });
 
 gulp.task('styles', function() {
     return gulp.src('src/styles/**/*.css')
            .pipe(plumber(plumberOpt))
            .pipe(gulp.dest('public/styles/'))
-           .pipe(livereload(reloadServer));
+           .pipe(livereload(lrPort));
 });
 
 gulp.task('serve', function() {
@@ -85,7 +84,6 @@ gulp.task('serve', function() {
     app.use(express.static('public'));
     app.use(express.static('bower_components'));
     app.listen(appPort);
-    reloadServer.listen(lrPort);
 });
 
 gulp.task('default_watch', function() {
@@ -112,8 +110,8 @@ gulp.task('default', ['html',
                       'default_watch']);
 
 gulp.task('caffeine', ['jade',
-                     'stylus',
-                     'coffee',
-                      'extern',
-                     'serve',
-                     'caffeine_watch']);
+                       'stylus',
+                       'coffee',
+                       'extern',
+                       'serve',
+                       'caffeine_watch']);
